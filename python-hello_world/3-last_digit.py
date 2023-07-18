e0f5644b8ddfe9be1a0db458d8c258f977561fcf
eedriz99 <akinsolaidris1999@gmail.com>
2023-07-18 03:58:37 +0100

    changes

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,5 +9,7 @@ if int(last_digit) > 5:
     print("Last digit of {} is {} and is greater than 5 \n".format(number, last_digit))
 elif int(last_digit) == 0:
     print("Last digit of {} is {} and is 0 \n".format(number, last_digit))
+elif int(last_digit) < 6 and number < 0:
+    print("Last digit of {} is -{} and is less than 6 and not 0 \n".format(number, last_digit))
 else:
     print("Last digit of {} is {} and is less than 6 and not 0 \n".format(number, last_digit))
--- python-hello_world/README.md
+++ python-hello_world/README.md
@@ -6,3 +6,4 @@ this directory is expected to contain 8 submission, which are:
 
 - 0-print.py: a Python script that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
 - 1-edges.py: A scripts that prints the first 3, last 2, and middle letters of a given word [excluding the first and last letters].
+- 2-positive_or_negative.py: A program that checks if a number is positive, negative or zero
