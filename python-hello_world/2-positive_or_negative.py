4606dfc3783449c5b164acaf2ec7f0bee007fee1
eedriz99 <akinsolaidris1999@gmail.com>
2023-07-18 15:48:48 +0100

    new chages

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
 if number > 0:
-    print("{} is positive \n".format(number))
+    print("{} is positive".format(number))
 elif number < 0:
-    print("{} is negative \n".format(number))
+    print("{} is negative".format(number))
 else:
-    print("{} is zero \n".format(number))
+    print("{} is zero".format(number))
