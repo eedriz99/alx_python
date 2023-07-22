861002955f4c9653114a99080d1be40f59dc2102
eedriz99 <akinsolaidris1999@gmail.com>
2023-07-22 19:04:10 +0100

    5-print_comb2.py corrected

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,8 +1,5 @@
-combo_list = []
 for num in range(100):
-    stringed_num = "{:0>2}".format(num)
     if num != 99:
-        if stringed_num.split("") not in combo_list:
-            combo_list.append(stringed_num.split(""))
-        print(int(stringed_num), end=", ")
-    
+        print("{:0>2}".format(num), end=", ")
+    else:
+        print(num)
