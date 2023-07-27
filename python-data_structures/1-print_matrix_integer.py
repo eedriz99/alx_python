8bd1419ee16b7cfb83ad90efc6c36a680be66e77
eedriz99 <akinsolaidris1999@gmail.com>
2023-07-27 01:47:50 +0100

    correction check

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,9 +1,9 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for cell in row:
-            if len(row) == 0:
-                print("{:d} \n".format(cell))
-            elif  row.index(cell) < len(row)-1:
+
+            if  row.index(cell) < len(row)-1:
                 print("{:d}".format(cell), end=" ")
             else:
-                print("{:d}".format(cell), end="\n")
+                print("{:d}".format(cell),)
+        print("\n")
