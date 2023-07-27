685a7f3e3ec380a5f6a20ed9db9ada375a1519b4
eedriz99 <akinsolaidris1999@gmail.com>
2023-07-27 01:35:47 +0100

    Task 1 str.format() corected

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,13 +2,8 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for cell in row:
             if  row.index(cell) < len(row)-1:
-                print("{}".format(cell), end=" ")
+                print("{:d}".format(cell), end=" ")
             else:
-                print("{}".format(cell), end="\n")
+                print("{:d}".format(cell), end="\n")
         # print("\n")
 
-print_matrix_integer(matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-])
