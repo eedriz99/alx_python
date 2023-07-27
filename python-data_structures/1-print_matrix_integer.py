76795a77255df00de637b7854d13ce7a9f8a5bc1
eedriz99 <akinsolaidris1999@gmail.com>
2023-07-27 01:43:11 +0100

    Task 1 last test corection check

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for cell in row:
-            if len(cell) == 0:
+            if len(row) == 0:
                 print("{:d}".format(cell), end=" \n")
             elif  row.index(cell) < len(row)-1:
                 print("{:d}".format(cell), end=" ")
